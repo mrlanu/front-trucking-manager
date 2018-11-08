a461363b158cec34d29dc2680fb45d71f28b03e6
Serhiy K <mrlanu@gmail.com>
2018-11-07 23:24:32 -0600

    Angular material added

---
+++ src/app/material.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {
+  MatFormFieldModule, MatIconModule, MatInputModule, MatDatepickerModule,
+  MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule,
+  MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
+  MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule,
+  MatPaginatorModule, MatSnackBarModule, MatAutocompleteModule, MatBadgeModule,
+  MatTooltipModule
+} from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule,
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
+    MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
+    MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule,
+    MatSortModule, MatPaginatorModule, MatSnackBarModule, MatAutocompleteModule,
+    MatBadgeModule, MatTooltipModule, MatExpansionModule
+  ],
+  exports: [
+    MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule,
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
+    MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
+    MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule,
+    MatSortModule, MatPaginatorModule, MatSnackBarModule,
+    MatAutocompleteModule, MatBadgeModule, MatTooltipModule, MatExpansionModule
+  ]
+})
+export class MaterialModule {
+
+}
